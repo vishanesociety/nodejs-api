9c10efde0ed5659de8e31bfce30d1f17b6304493
Kshitij Kumar Singh <ksingh@burhanitechnologies.com>
2022-02-03 21:51:27 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3500
 
+app.get('/', (req,res) => {
+    res.send('Hello World!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
